268b592255dc38f5083ab8aa312164ae69acf50a
Walace <walacecastro@live.com>
2023-09-19 14:30:40 -0300

    Criando primeiro Controller

---
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -0,0 +1,19 @@
+package com.algaworks.algafood;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+//@Controller informa que a classe é responsável por receber requisições WEB.
+@Controller
+public class MeuPrimeiroController {
+
+    //@GetMapping indica qual o caminho para chegar no método
+    @GetMapping("/hello")
+    //@ResponseBody indica que queremos que a String seja devolvida como resposta para requisição.
+    @ResponseBody
+    public String hello() {
+        return "Hello";
+    }
+
+}
