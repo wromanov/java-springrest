b6516ba05066969aac7f4a60bb05075843c94205
walacedelgado <walacecastro@live.com>
2023-09-19 20:06:50 -0300

    Atualizando Controller

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -7,11 +7,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 //@Controller informa que a classe é responsável por receber requisições WEB.
 @Controller
 public class MeuPrimeiroController {
-
-    //@GetMapping indica qual o caminho para chegar no método via web.
-    @GetMapping("/hello")
-    //@ResponseBody indica que queremos que a String seja devolvida como resposta para requisição.
-    @ResponseBody
+    @GetMapping("/hello") //@GetMapping indica qual o caminho para chegar no método via web.
+    @ResponseBody //@ResponseBody indica que queremos que a String seja devolvida como resposta para requisição.
     public String hello() {
         return "Oi";
     }
